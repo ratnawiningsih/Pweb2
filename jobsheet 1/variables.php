359d38a97d407c3307f7e088959a7870ec88e548
ratnawiningsih <132966390+ratnawiningsih@users.noreply.github.com>
2023-10-02 22:10:32 +0700

    Create variables.php
    jobsheet 1

---
+++ jobsheet 1/variables.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php
+    $integer= 26;
+    $string= "PWEB";
+    $bolean= true;
+
+    echo "string= $string </br>";
+    echo "integer= $integer </br>";
+    echo "bolean= $bolean</br>";
+    ?>
+</body>
+</html>
